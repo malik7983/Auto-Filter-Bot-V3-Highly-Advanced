35b6f9008263bd80b7198f172230250554cfea7d
Joelkb <92435259+Joelkb@users.noreply.github.com>
2021-10-30 11:20:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,8 +19,6 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('⚙️Support Group⚙️', url='https://t.me/joinchat/8i064A8O6zYzZWY1')
             ],[
